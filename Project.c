3ebd57aa87066bdc6af8ad3a36fc8d8e4f4f2045
Pramish-Aryal <60599905+Pramish-Aryal@users.noreply.github.com>
2020-02-10 19:47:39 +0545

    Add files via upload

--- Project.c
+++ Project.c
@@ -6,18 +6,18 @@
 	int height=20;
 	int width=50;
 	bool gameover;
-	int x,y,score;
+	int x,y,score;  //x,y are pacman's co-ortinates
 	enum eDirection{STOP=0, LEFT,RIGHT,UP,DOWN};
 	enum eDirection dir;
-	
-	
+
+
 	void Setup(){
 		gameover=false;
 		dir=STOP;
 		x = width / 2;
 		y = height / 2;
 		score=0;
-		
+
 	}
 
 	void Draw(){
@@ -26,16 +26,17 @@
 		for (i=0;i<width;i++)
 		{
 			if(i==0) printf("%c",201);
-			else if(i==width-1) printf("%c",187);
+			else if(i==width-1) printf("%c",187);           //prints top border
 			else printf("%c",205);
 		}
-	
+
 		printf("\n");
 		for(i=0;i<height;i++){
 			for(j=0;j<width;j++){
-				if(j==0 || j==width-1) printf("%c",186);
+				if(j==0 || j==width-1) printf("%c",186);        //prints side border, pacman(C), "dot(.)",,,
+                                                                //add ghosts too
 				else if(j==x && i==y) printf("C");
-				
+
 				else printf(".");
 
 			}
@@ -45,42 +46,42 @@
 		for (i=0;i<width;i++)
 		{
 			if(i==0) printf("%c",200);
-			else if(i==width-1) printf("%c",188);
+			else if(i==width-1) printf("%c",188);           //lower border
 			else printf("%c",205);
 		}
-		
+
 	}
-	
+
 	void Input(){
 		if(_kbhit()){
-			switch(_getch()){
+			switch(_getch()){                           //if user presses stuff then only getch() activates and hence takes input
 				case 'a':
 					dir=LEFT;
 					break;
-				
+
 				case 'd':
 					dir=RIGHT;
 					break;
-				
+
 				case 'w':
 					dir=UP;
 					break;
-				
-			
+
+
 				case 's':
 					dir=DOWN;
-					break;	
-					
+					break;
+
 				case 'x':
 					gameover=true;
 					break;
 			}
 		}
 	}
-	
+
 	void Logic(){
-	
-			switch(dir){
+
+			switch(dir){                            //pacman's co-ordinate changes when corresponding key is pressed
 				case LEFT:
 					x--;
 					break;
